3d280db32c1cf7a6002777176f0fc1125917d1c3
DungyeuHang <nguyendungtvhy@gmail.com>
2026-08-11 10:38:31 +0700

    chore: remove temporary fix scripts

--- _fix_dialog.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const p = 'd:/GitHub/english-platform/src/shared/components/ConfirmationDialog.tsx';
-let c = fs.readFileSync(p, 'utf8');
-c = c.replace("import ReactNode from 'react';\n", '');
-fs.writeFileSync(p, c, 'utf8');
-console.log('ok');
